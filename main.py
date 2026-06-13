7a1950102f712505d791f37d27ddb87a7a963773
aryavengurlekar03 <aryavengurlekar03@gmial.com>
2026-06-13 12:07:19 +0530

    Added PDF resume extraction

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("resume analyzer")
+from extractor import extract_text
+
+resume_path = "resume/Arya Vengurlekar Resume.pdf"
+
+resume_text = extract_text(resume_path)
+
+print("=" * 50)
+print("RESUME TEXT")
+print("=" * 50)
+
+print(resume_text)
